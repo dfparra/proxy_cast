9f0528b17beb6751ee10fa1ed29f8622eebb8be0
Daniel <danielparra93@gmail.com>
2016-07-20 18:45:47 -0400

    added data parsing for the daily route

--- index.js
+++ index.js
@@ -35,22 +35,54 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
 });
 
 server.get('/forecast/daily/:lat,:lon', function(req, res){
-  $http.get(baseUrl+ apiKey+'/'+req.params.lat+','+req.params.lon)
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
         .then(function(response){
+          var overSummary = response.data.daily.summary;
+          var overIcon = response.data.daily.icon;
+          var dailyData = response.data.daily.data;
+          var dailyArr = [];
+          for(var i = 0; i < dailyData.length; i+=1){
+            var o = {
+              icon: dailyData[i].icon,
+              tempMax: dailyData[i].temperatureMax,
+              tempMin: dailyData[i].temperatureMin,
+              humidity: dailyData[i].humidity,
+              precipProb: dailyData[i].precipProbability
+            };
+            dailyArr.push(o);
+          }
+
           var resObj = {
             latitude: response.data.latitude,
             longitude: response.data.longitude,
-            daily: response.data.daily,
-
+            summary: overSummary,
+            icon: overIcon,
+            daily: dailyArr
           };
-          res.status(200).json(resObj); //The respnse was succesful and here is the info
+          res.status(200).json(resObj);
         })
-        .catch(function(error){
-          // console.log(error);
+        .catch(function(err){
           res.status(500).json({
-            msg: error
+            msg: err
           });
         });
+  // $http.get(baseUrl+ apiKey+'/'+req.params.lat+','+req.params.lon)
+  //       .then(function(response){
+  //         var resObj = {
+  //           latitude: response.data.latitude,
+  //           longitude: response.data.longitude,
+  //           daily1: response.data.daily.data[0].time,
+  //           daily2:response.data.daily.data[1].time,
+  //
+  //         };
+  //         res.status(200).json(resObj); //The respnse was succesful and here is the info
+  //       })
+  //       .catch(function(error){
+  //         // console.log(error);
+  //         res.status(500).json({
+  //           msg: error
+  //         });
+  //       });
 });
 
 server.get('/forecast/minutely/:lat,:lon', function(req, res){
